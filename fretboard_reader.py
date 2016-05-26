c09b3cc9e6467a888c48ae249cb46acc388e49d6
Nathello <chrisdrydenwork@gmail.com>
2016-05-26 23:18:11 +0100

    Note finder first pass

--- fretboard_reader.py
+++ fretboard_reader.py
@@ -1,3 +1,40 @@
 class FretboardReader:
 	def __init__(self):
-		pass
+		self.standard_tuning = ["E","A","D","G","B","E"]
+		self.notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+
+	def generate_notes(self, fretboard_values):
+		result_array = []
+		if len(fretboard_values) < len(self.standard_tuning):
+			return result_array
+
+		for index, value in enumerate(self.standard_tuning):
+			tuning_result = fretboard_values[index].lower()
+			if tuning_result == "x":
+				continue
+			note = self.find_note(tuning_result, value)
+			result_array.append(note)
+
+		return result_array
+
+	def find_note(self, fretboard_position, open_note):
+		fretted_note_index = self.fretted_note_index(fretboard_position, open_note)
+		fretted_note = self.notes[fretted_note_index]
+		return fretted_note
+
+	def fretted_note_index(self, fretboard_position, open_note):
+		fret_number = int(fretboard_position)
+		note_index = self.notes.index(open_note)
+		fretted_note_index = fret_number + note_index
+		valid_fretted_note_index = self.valid_note_index(fretted_note_index)
+		return valid_fretted_note_index
+
+	def valid_note_index(self, fretted_note_index):
+		number_of_notes = len(self.notes)
+		if fretted_note_index < number_of_notes - 1:
+			return fretted_note_index
+
+		overflow_times = int(fretted_note_index / number_of_notes)
+		multiplier = overflow_times * number_of_notes
+
+		return fretted_note_index - multiplier
--- test_fretboard_reader.py
+++ test_fretboard_reader.py
@@ -1,4 +1,58 @@
 from fretboard_reader import FretboardReader
-def test_init():
-    reader = FretboardReader()
-    assert reader is not None
+
+import unittest
+
+class TestFretboardReader(unittest.TestCase):
+
+	def setUp(self):
+		self.reader = FretboardReader()
+
+	def test_init(self):
+	    self.assertIsNotNone(self.reader)
+
+	def test_standard_tuning(self):
+		result = self.reader.standard_tuning
+		self.assertEquals(result, ["E","A","D","G","B","E"])
+
+	def test_notes(self):
+		result = self.reader.notes
+		self.assertEquals(result, ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"])
+
+	def test_generate_notes_with_invalid_note_quantity_returns_empty(self):
+		stupid_array = ["x","x","x","x"]
+		result = self.reader.generate_notes(stupid_array)
+		self.assertEquals(result, [])
+
+	def test_generate_notes_with_x_lower_returns_empty_array(self):
+		stupid_array = ["x","x","x","x","x","x"]
+		result = self.reader.generate_notes(stupid_array)
+		self.assertEquals(result, [])
+
+	def test_generate_notes_with_x_upper_returns_empty_array(self):
+		stupid_array = ["X","x","X","x","X","x"]
+		result = self.reader.generate_notes(stupid_array)
+		self.assertEquals(result, [])
+
+	def test_fretted_note_index_returns_correct_index__0(self):
+		result = self.reader.fretted_note_index("0", "E")
+		self.assertEquals(result, 7)
+
+	def test_fretted_note_index_returns_correct_index__2(self):
+		result = self.reader.fretted_note_index("2", "E")
+		self.assertEquals(result, 9)
+
+	def test_valid_note_index_less_than_max_length_of_notes(self):
+		result = self.reader.valid_note_index(2)
+		self.assertEquals(result, 2)
+
+	def test_valid_note_index_last_item_plus_1(self):
+		result = self.reader.valid_note_index(12)
+		self.assertEquals(result,0)
+
+	def test_valid_note_index_last_item_plus_5(self):
+		result = self.reader.valid_note_index(16)
+		self.assertEquals(result,4)
+
+	def test_valid_note_index_last_item_plus_15(self):
+		result = self.reader.valid_note_index(26)
+		self.assertEquals(result,2)
