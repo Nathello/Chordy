1b76f19fc400df48dafaf4e5738e76d9eb62b5d4
Valerie Gibson <vgibson14@gmail.com>
2016-06-08 22:44:16 +0100

    now with added bawbag

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from fretboard_reader import FretboardReader
+
 class Main:
-	def __init__(self):
-		
+  def __init__(self):
+    reader = FretboardReader()
+    fretboard_input = raw_input("Gimme yo damn fretboard positions biatch: ")
+
+    while fretboard_input != "q":
+
+      array = fretboard_input.split(" ")
+      print reader.generate_notes(array)
+      fretboard_input = raw_input("Gimme yo damn fretboard positions biatch: ")
+
+    print "see ya later pal"
+
+
+Main()
