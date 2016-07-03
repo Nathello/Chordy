b698329f6d4b35174f115577004f6a79bab66d8d
Valerie Gibson <vgibson14@gmail.com>
2016-07-03 16:53:28 +0100

    Manual  note interval array created

--- note_lookup.py
+++ note_lookup.py
@@ -2,5 +2,12 @@ class NoteLookup:
   def __init__(self):
     self.notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
 
-  def note_index(self,note):
-    return self.notes.index(note)
+  def note_index(self,note_input):
+    counter = 0
+    for note in self.notes:
+      if note == note_input:
+        return counter
+      counter += 1
+    return -1
+
+    
