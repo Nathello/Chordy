0cc5d95fec868f90a2e65ce1fd1680e2c6c7f56d
Valerie Gibson <vgibson14@gmail.com>
2016-07-03 17:52:57 +0100

    oh dear

--- note_lookup.py
+++ note_lookup.py
@@ -6,26 +6,25 @@ class NoteLookup:
     return self.notes.index(note)
 
   def note_interval(self, root_note_index, note_to_find):
-    interval = 0
+    self.interval = 0
 
-    current_location = root_note_index
-    while (current_location < len(self.notes)):
-      current_note = self.notes[current_location]
-      if note_to_find == current_note:
-        return interval
-      interval += 1
-      current_location += 1
+    self.current_location = root_note_index
 
-    current_location = 0
-    while (current_location < root_note_index):
-      current_note = self.notes[current_location]
-      if note_to_find == current_note:
-        return interval
-      interval += 1
-      current_location += 1
+    while (self.current_location < len(self.notes)):
+      found = self.match(note_to_find)
+      if found: return self.interval
 
+    self.current_location = 0
+
+    while (self.current_location < root_note_index):
+      found = self.match(note_to_find)
+      if found: return self.interval
 
     return -1
 
-
-
+  def match(self, note_to_find):
+    current_note = self.notes[self.current_location]
+    if note_to_find == current_note:
+      return True
+    self.interval += 1
+    self.current_location += 1
