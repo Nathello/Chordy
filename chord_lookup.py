c8ce08a67aceb5c85ef09de009d35a83d59a52bb
Valerie Gibson <vgibson14@gmail.com>
2016-07-06 11:47:12 +0100

    adds dominant seventh

--- chord_lookup.py
+++ chord_lookup.py
@@ -6,7 +6,8 @@ class ChordLookup:
       "0-4-7-11":"Major Seventh",
       "0-2-7":"Suspended Second",
       "0-5-7":"Suspended Fourth",
-      "0-3-7-10":"Minor Seventh"
+      "0-3-7-10":"Minor Seventh",
+      "0-4-7-10":"Dominant Seventh"
     }
 
   def convert_to_key(self, intervals_array):
